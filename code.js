b74f1b35656db9d7f1feebc6b962711fe82276bd
MickeyBoy <olanrewajuabdulrazaq5@gmail.com>
2021-05-05 10:35:48 +0100

    first commit

---
+++ code.js
@@ -0,0 +1,9 @@
+var list = prompt("Enter your user in the following users \n 1:Ayomide \n 2:Aregbe \n 3:Adepoju \n ");
+var user = prompt("Enter your username here");
+var pass = prompt("Enter your password here");
+ if (list == 1 || list == 2 || list == 3 && user == "user" && pass == "pass"){
+     alert("Welcome")
+ } else {
+     alert("Invalid username and pasword")
+}
+    
